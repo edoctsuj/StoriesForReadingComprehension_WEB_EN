bb2f932a99867901753af6588bf9e0d868eca26b
zawkhantmaung-loftal <zawkhantmaung@outlook.com>
2023-09-14 01:35:49 +0630

    .

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 # Import Classes from Libraries
 import logging
 import sys
@@ -9,13 +8,15 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 from llama_index import TrafilaturaWebReader
+import streamlit as st
+from streamlit_chat import message
 
 
 # Load environment values
 load_dotenv()
 
 # Initialize OpenAI LLM
-llm = OpenAI(model_name="text-davinci-003") #we can also use 'text-davinci-002'
+llm = OpenAI(model_name="text-davinci-003")  # we can also use 'text-davinci-002'
 
 # Read Data From WebPage
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -45,197 +46,82 @@ documentSearch = FAISS.from_texts(finalData, embeddings)
 # Load QA chain
 chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
+# Setting page title and header
+st.set_page_config(page_title="PigeonCloud bot", page_icon=":robot_face:")
+
+# Read data from web
+st.sidebar.markdown(
+    "<h1 style='text-align: center;'>PigeonCloud Support</h1>", unsafe_allow_html=True
+)
+st.sidebar.info(
+    "Chatbot, that reads data from a web page and answers questions based on the information it retrieves."
+)
+
+# Read data from web
+st.sidebar.title("Read Data From Web😎")
+st.sidebar.text("https://edoctsuj.github.io/demo_data/")
+
+# Sample questions to ask
+st.sidebar.title("Sample Questions😎")
+st.sidebar.text("Did Mr Jones work in George's shop?")  # No, he didn't
+st.sidebar.text("Who worked in Mr Jones's shop?")  # George did.
+st.sidebar.text("How many awards did Huhu won?")
+st.sidebar.text("Did the shop sell tables, or food?")  # It sold food
+st.sidebar.text("Were Gladys's father and mother rich?")  # No, they were not
+st.sidebar.text("Did Gladys want to work in her small, quiet town?")  # No, she did not
+st.sidebar.text("How many awards did Huhu won?")
+st.sidebar.text("Who did Gladys want to marry?")  # A rich man
+st.sidebar.text("Was Alan's shop open on Monday?")  # No, it wasn't
+st.sidebar.text("Was Alan's shop shut on Saturday?")  # No, it wasn't
+st.sidebar.text("How many awards did Huhu won?")
+st.sidebar.text("Was Alan's shop open or shut on Sunday morning?")  # It was open
+st.sidebar.text("Did Mr and Mrs Brown have any children?")  # Yes, they had four.
+st.sidebar.text("Did their children marry?")  # Yes, all of them did
+st.sidebar.text("How many awards did Huhu won?")
+st.sidebar.text("Did they have any grandsons?")  # Two
+st.sidebar.text("What was Joe's father?")  # He was a farmer.
+st.sidebar.text("Was his father rich?")  # No, he was not
+st.sidebar.text("How many awards did Huhu won?")
+st.sidebar.text(
+    "Why did Joe leave his father's farm?"
+)  # Because he wanted a farm in a better place.
 
 
+# Set empty array
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
 
-# Q&A
+# Get Response
+def getresponse(question):
+    docs = documentSearch.similarity_search(question)
+    answer = chain.run(input_documents=docs, question=question)
 
-# Unit_1
-# Q1:Perform document search and question answering
-our_query = "Did Mr Jones work in George's shop?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A1:Print answer
-print(our_query)
-print(answer) #No, he didn't
-
-# Q2:Perform document search and question answering
-our_query = "Who worked in Mr Jones's shop?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A2:Print answer
-print(our_query)
-print(answer) # George did. 
-
-# Unknow Question For AI
-# Q3:Perform document search and question answering
-our_query = "How many awards did Huhu won?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A3:Print answer
-print(our_query)
-print(answer)
-
-# Q4:Perform document search and question answering
-our_query = "Did the shop sell tables, or food?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A4:Print answer
-print(our_query)
-print(answer) # It sold food
+    return answer
 
 
+# Conversation
+response_container = st.container()
+container = st.container()
 
+with container:
+    with st.form(key="my_form", clear_on_submit=True):
+        user_input = st.text_area("Your question goes here:", key="input", height=100)
+        submit_button = st.form_submit_button(label="Send")
 
+        if submit_button:
+            st.session_state["messages"].append(user_input)
 
-# Unit_2
-# Q1:Perform document search and question answering
-our_query = "Were Gladys's father and mother rich?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A1:Print answer
-print(our_query)
-print(answer) #No, they were not
+            model_response = getresponse(user_input)
+            st.session_state["messages"].append(model_response)
 
-# Q2:Perform document search and question answering
-our_query = "Did Gladys want to work in her small, quiet town?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A2:Print answer
-print(our_query)
-print(answer) # No, she did not
-
-# Unknow Question For AI
-# Q3:Perform document search and question answering
-our_query = "How many awards did Huhu won?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A3:Print answer
-print(our_query)
-print(answer)
-
-# Q4:Perform document search and question answering
-our_query = "Who did Gladys want to marry?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A4:Print answer
-print(our_query)
-print(answer) #A rich man
-
-
-
-
-
-
-# Unit_3
-# Q1:Perform document search and question answering
-our_query = "Was Alan's shop open on Monday?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A1:Print answer
-print(our_query)
-print(answer) # No, it wasn't
-
-# Q2:Perform document search and question answering
-our_query = "Was Alan's shop shut on Saturday?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A2:Print answer
-print(our_query)
-print(answer) #No, it wasn't
-
-# Unknow Question For AI
-# Q3:Perform document search and question answering
-our_query = "How many awards did Huhu won?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A3:Print answer
-print(our_query)
-print(answer)
-
-# Q4:Perform document search and question answering
-our_query = "Was Alan's shop open or shut on Sunday morning?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A4:Print answer
-print(our_query)
-print(answer) # It was open
-
-
-
-
-
-
-# Unit_4
-# Q1:Perform document search and question answering
-our_query = "Did Mr and Mrs Brown have any children?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A1:Print answer
-print(our_query)
-print(answer) # Yes, they had four.
-
-# Q2:Perform document search and question answering
-our_query = " Did their children marry?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A2:Print answer
-print(our_query)
-print(answer) #Yes, all of them did
-
-# Unknow Question For AI
-# Q3:Perform document search and question answering
-our_query = "How many awards did Huhu won?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A3:Print answer
-print(our_query)
-print(answer)
-
-# Q4:Perform document search and question answering
-our_query = "Did they have any grandsons?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A4:Print answer
-print(our_query)
-print(answer) #Two
-
-
-
-
-
-
-# Unit_5
-# Q1:Perform document search and question answering
-our_query = "What was Joe's father?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A1:Print answer
-print(our_query)
-print(answer) #He was a farmer.
-
-# Q2:Perform document search and question answering
-our_query = "Was his father rich?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A2:Print answer
-print(our_query)
-print(answer) #No, he was not
-
-# Unknow Question For AI
-# Q3:Perform document search and question answering
-our_query = "How many awards did Huhu won?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A3:Print answer
-print(our_query)
-print(answer)
-
-# Q4:Perform document search and question answering
-our_query = "Why did Joe leave his father's farm?"
-docs = documentSearch.similarity_search(our_query)
-answer = chain.run(input_documents=docs, question=our_query)
-# A4:Print answer
-print(our_query)
-print(answer) #Because he wanted a farm in a better place.
+            with response_container:
+                for i in range(len(st.session_state["messages"])):
+                    if (i % 2) == 0:
+                        message(
+                            st.session_state["messages"][i],
+                            is_user=True,
+                            key=str(i) + "_user",
+                        )
+                    else:
+                        message(st.session_state["messages"][i], key=str(i) + "_AI")
